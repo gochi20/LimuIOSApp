5c57a5ffca6865ec42a49a672d2064143a72451e
chigo <chigomezgon@gmail.com>
2026-06-18 15:09:13 +0200

    Initial Commit

---
+++ Limu Mobile/Limu_MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  Limu_MobileApp.swift
+//  Limu Mobile
+//
+//  Created by Apple Guy on 18/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct Limu_MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
